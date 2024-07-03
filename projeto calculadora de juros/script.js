4f341bb957b7da2a6281bd445ca21fab579114a3
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2024-07-03 20:17:09 -0300

    pagina sobre terminada

--- projeto calculadora de juros/script.js
+++ projeto calculadora de juros/script.js
@@ -1,3 +1,9 @@
-var capital = parseFloat(get)
-var taxa = 
-var tempo =
+function abrirMenu(){
+    document.getElementById("menu-lateral").style.width="250px";
+    document.getElementById("principal").style.marginLeft="250px";
+}
+
+function fecharMenu(){
+    document.getElementById("menu-lateral").style.width="0";
+    document.getElementById("menu-lateral").style.marginLeft="0";
+}
