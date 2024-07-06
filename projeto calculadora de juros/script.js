191c04590224c4341a05fcc1c70a50c240ad378c
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2024-07-06 11:38:58 -0300

    script js funcionando

--- projeto calculadora de juros/script.js
+++ projeto calculadora de juros/script.js
@@ -1,9 +1,45 @@
-function abrirMenu(){
-    document.getElementById("menu-lateral").style.width="250px";
-    document.getElementById("principal").style.marginLeft="250px";
+function abrirMenu() {
+    document.getElementById("menu-lateral").style.width = "250px";
+    document.getElementById("principal").style.marginLeft = "250px";
 }
 
-function fecharMenu(){
-    document.getElementById("menu-lateral").style.width="0";
-    document.getElementById("menu-lateral").style.marginLeft="0";
+function fecharMenu() {
+    document.getElementById("menu-lateral").style.width = "0";
+    document.getElementById("principal").style.marginLeft = "0";
+}
+
+function calculaJurosSimples() {
+    var capital = parseFloat(document.getElementById('capital').value);
+    var taxa = parseFloat(document.getElementById('taxa').value);
+    var tempo = parseFloat(document.getElementById('tempo').value);
+
+    if (isNaN(capital) || isNaN(taxa) || isNaN(tempo)) {
+        alert('Por favor, insira valores válidos.');
+        return;
+    }
+
+    var juros = (capital * taxa * tempo) / 100;
+    var montante = capital + juros;
+
+    document.getElementById('mostraCapital').innerText = 'R$ ' + capital.toFixed(2);
+    document.getElementById('mostraJuros').innerText = 'R$ ' + juros.toFixed(2);
+    document.getElementById('mostraMontante').innerText = 'R$ ' + montante.toFixed(2);
+}
+
+function calculaJurosCompostos(){
+    var capital = parseFloat(document.getElementById('capital').value);
+    var taxa = parseFloat(document.getElementById('taxa').value);
+    var tempo = parseFloat(document.getElementById('tempo').value);
+
+    if (isNaN(capital) || isNaN(taxa) || isNaN(tempo)) {
+        alert('Insira valores válidos');
+        return;
+    }
+
+    var montante = capital * Math.pow((taxa / 100 + 1), tempo);
+    var juros = montante - capital;
+    
+    document.getElementById('mostraCapital').innerText = 'R$ ' + capital.toFixed(2);
+    document.getElementById('mostraJuros').innerText = 'R$ ' + juros.toFixed(2);
+    document.getElementById('mostraMontante').innerText = 'R$ ' + montante.toFixed(2);
 }
